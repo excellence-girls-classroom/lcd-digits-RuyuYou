7d5699adce0bc9754886eb65b3b5de564996f55f
Ruyu You <727809166@qq.com>
2016-04-21 14:14:20 +0800

    UnitTest of stringSplit

--- test/main_test.js
+++ test/main_test.js
@@ -1,3 +1,22 @@
-/**
- * Created by you on 16-4-19.
- */
+describe('Unit Test',function () {
+    var inputs;
+    var allItems;
+
+    beforeEach(function () {
+        allItems = loadAllItems ();
+        inputs = "910";
+    });
+
+    describe('stringSplit()',function () {
+        it('should print correct string',function () {
+            var newInputs = stringSplit (inputs);
+            var expectNewInputs = [
+                '9',
+                '1',
+                '0'
+            ];
+            expect(newInputs).toEqual(expectNewInputs);
+        });
+    });
+
+});
