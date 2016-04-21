ec9758df357c2d3a01e08e9d18bcaad1a40665cb
Ruyu You <727809166@qq.com>
2016-04-21 14:56:58 +0800

    write function buildItems

--- main/main.js
+++ main/main.js
@@ -1,8 +1,23 @@
 function printLcdDisplay (inputs) {
+    var allItems = loadAllItems();
     var newInputs = stringSplit(inputs);
+    console.log (allItems);
+    var items = buildItems(newInputs , allItenms);
 }
 
 function stringSplit(inputs) {
     var newInputs = inputs.split('');
     return newInputs;
+}
+
+function buildItems(newInputs , allItems) {
+    var items = [];
+    for (var i=0; i<newInputs.length; i++) {
+        for (var j=0; j<allItems.length; j++) {
+            if (newInputs[i] === allItems[j].number) {
+                items.push ({ number:allItems[j].number ,lcd:allItems[j].lcd });
+            }
+        }
+    }
+    return items;
 }
